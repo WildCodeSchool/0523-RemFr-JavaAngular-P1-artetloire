68788f97ae6e7e266cd428602584e5f245fe35b9
Matthieu <Matthieu.Maginot@gmail.com>
2023-06-07 09:01:08 +0200

    carte + emplacement musée

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MarkerService } from './services/marker.service';
+
 import { DemoPageComponent } from './pages/demo/demo.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { DemoPipe } from './pipes/demo.pipe';
 import { DemoDirective } from './directives/demo.directive';
+import { CarteComponent } from './pages/carte/carte.component';
 
 
 @NgModule({
@@ -17,14 +21,18 @@ import { DemoDirective } from './directives/demo.directive';
     DemoPageComponent,
     DemoComponent,
     DemoPipe,
-    DemoDirective
+    DemoDirective,
+    CarteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MarkerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/carte/carte.component.ts
@@ -0,0 +1,49 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import { MarkerService } from '../../services/marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
+@Component({
+  selector: 'app-carte',
+  templateUrl: './carte.component.html',
+  styleUrls: ['./carte.component.scss']
+})
+export class CarteComponent implements AfterViewInit {
+  private map!: L.Map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 47.4130666, 0.9839873 ],  
+      zoom: 14
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor(private markerService: MarkerService) { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    this.markerService.makeCapitalMarkers(this.map);
+  }
+}
