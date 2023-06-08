ffa377353e32abd6c8b63d031556ee44e00edc18
Daniel Thibaut <dthibaut1@free.fr>
2023-06-08 12:25:49 +0200

    Suppress demo directive/pipe, update demo page for test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DemoPageComponent } from './pages/demo/demo.component';
-import { DemoPipe } from './pipes/demo.pipe';
-import { DemoDirective } from './directives/demo.directive';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 
@@ -15,8 +13,6 @@ import { NavigationComponent } from './components/navigation/navigation.componen
   declarations: [
     AppComponent,
     DemoPageComponent,
-    DemoPipe,
-    DemoDirective,
     NavigationComponent
   ],
   imports: [
