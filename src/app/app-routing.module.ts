c91d408ee5c93a63f1895f826ca243c5d1347255
Daniel Thibaut <dthibaut1@free.fr>
2023-06-08 09:52:42 +0200

    My first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { DemoPageComponent } from './pages/demo/demo.component';
 
 const routes: Routes = [
-  { path: '', component: DemoPageComponent }
+  { path: '', component: DemoPageComponent },
+  // { path: 'recherche', component: RechercheComponent},
+  // { path: 'recherche', component: RechercheComponent},
 ];
 
 @NgModule({
