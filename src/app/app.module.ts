a1e3d5208cebb1414fc50cc275c118c14b69aa8a
Matthieu <Matthieu.Maginot@gmail.com>
2023-06-08 11:29:58 +0200

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MarkerService } from './services/marker.service';
 
-import { DemoPageComponent } from './pages/demo/demo.component';
-import { DemoComponent } from './components/demo/demo.component';
-import { DemoPipe } from './pipes/demo.pipe';
-import { DemoDirective } from './directives/demo.directive';
 import { MapComponent } from './pages/map/map.component';
 import { ModalMapComponent } from './components/modal-map/modal-map.component';
 
@@ -19,10 +15,6 @@ import { ModalMapComponent } from './components/modal-map/modal-map.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DemoPageComponent,
-    DemoComponent,
-    DemoPipe,
-    DemoDirective,
     MapComponent,
     ModalMapComponent
   ],
