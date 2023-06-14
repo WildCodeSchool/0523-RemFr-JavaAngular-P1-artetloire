45d18f6ec11c7bcd982ad0ebfc1ddec4ae4c549e
Kuroshin28 <baptiste.bertinWOT@gmail.com>
2023-06-14 14:14:33 +0200

    Add some content : - Add ID and class to carrousel element to better integrate navigation - Using ID control based on total element - Need to fix dynamic navigation on carousel - Add some CSS modification due to some class and ID addition - Add comment in HTML for future integration

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carrousel',
   templateUrl: './carrousel.component.html',
   styleUrls: ['./carrousel.component.scss']
 })
-export class CarrouselComponent {
+export class CarrouselComponent implements OnInit {
+  Index: number = 1;
 
+  ngOnInit() {
+    this.Carrousel();
+  }
+
+  Carrousel() {
+    setTimeout(() => {
+      document.getElementById('nav-' + this.Index)?.click();
+      this.Index++;
+      if (this.Index > document.getElementsByClassName("slider-element").length) {
+        this.Index = 1;
+      }
+      this.Carrousel();
+    }, 5000);
+  }
 }
