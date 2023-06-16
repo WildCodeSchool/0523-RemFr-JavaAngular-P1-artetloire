df99680b3ff7dbc3eda8cd9cf3f533d2dee3d915
Kuroshin28 <baptiste.bertinWOT@gmail.com>
2023-06-16 14:51:05 +0200

    Add modification to carousel : - Vertical scroll issue solved - Add accueil service - Api call in accueil page - Dynamic display in html

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -9,7 +9,10 @@ export class CarouselComponent implements OnInit {
   Index: number = 1;
   Timeout: any = 0; // Change this you little piece of shit !
 
-  ngOnInit() {
+  @Input()
+  museumSample: any[] = [];
+
+  /*ngOnInit() {
     this.Carrousel();
   }
 
@@ -28,5 +31,35 @@ export class CarouselComponent implements OnInit {
     clearTimeout(this.Timeout);
     this.Index = id;
     this.Carrousel();
+  }*/
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.Timeout);
+  }
+
+  startCarousel() {
+    this.Timeout = setTimeout(() => {
+      this.Index++;
+      if (this.Index > document.getElementsByClassName("slider-element").length) {
+        this.Index = 1;
+      }
+      this.manualTravel(this.Index);
+    }, 5000);
+  }
+
+  manualTravel(id: number) {
+    clearTimeout(this.Timeout);
+    this.Index = id;
+    const element = document.getElementById('slider-element-' + id);
+    if (element) {
+      const Yreset = window.scrollY;
+      element.scrollIntoView();
+      window.scrollTo(0, Yreset);
+    }
+    this.startCarousel();
   }
 }
--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccueilService } from 'src/app/services/accueil.service';
 
 @Component({
   selector: 'app-accueil-page',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.scss']
 })
-export class AccueilPageComponent {
+export class AccueilPageComponent implements OnInit {
+  museumList: any[] = [];
+  museumSample: any[] = [];
 
+  constructor(private dataSet : AccueilService) {
+    
+  }
+
+  ngOnInit() {
+    this.dataSet.getAll().subscribe((museums: any[]) => {
+      this.museumList = museums;
+  
+      while (this.museumSample.length != 4) {
+        let tempElement: any = this.museumList[Math.floor(Math.random() * this.museumList.length)];
+        if (!this.museumSample.some((element: any) => element.uuid === tempElement.uuid)) {
+          this.museumSample.push(tempElement);
+        }
+      }
+    });
+  }
 }
