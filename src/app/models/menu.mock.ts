7ce57256a05ac073894347de107b5835fe5b18f7
Daniel Thibaut <dthibaut1@free.fr>
2023-06-21 14:52:14 +0200

    suppress icons path + change path if necessary

--- src/app/models/menu.mock.ts
+++ src/app/models/menu.mock.ts
@@ -1,10 +1,10 @@
 import { Menu } from "./menu.model";
 
-const accueil = new Menu('Accueil', '/', 'assets/icons/accueil.png')
-const search = new Menu('Recherche', '/search', 'assets/icons/recherche2.png')
-const map = new Menu('Carte', '/map', 'assets/icons/carte.png')
-const visites = new Menu('Visites', '/favorite', 'assets/icons/favorite.png')
-const aPropos = new Menu('A Propos', '/support', 'assets/icons/a_propos.png')
+const accueil = new Menu('Accueil', '/', 'assets/icon/accueil.png')
+const search = new Menu('Recherche', '/search', 'assets/icon/recherche2.png')
+const map = new Menu('Carte', '/map', 'assets/icon/carte.png')
+const visites = new Menu('Visites', '/favorite', 'assets/icon/favorite.png')
+const aPropos = new Menu('A Propos', '/support', 'assets/icon/a_propos.png')
 
 export const MENU_LIST: Menu[] = [
   accueil,
