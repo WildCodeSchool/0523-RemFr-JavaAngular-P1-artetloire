9fa7d5985d616eac5a57419d5b616dbcc11a658b
Daniel Thibaut <dthibaut1@free.fr>
2023-06-23 11:31:25 +0200

    Add all dev of menu burger and update header

--- src/app/components/menu-burger/menu-burger.component.ts
+++ src/app/components/menu-burger/menu-burger.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderParam, LIST_HEADER_VALUES } from 'src/app/models/header.model';
+import { Menu } from 'src/app/models/menu.model';
+import { MenuService } from 'src/app/services/menu.service';
 
 @Component({
   selector: 'app-menu-burger',
   templateUrl: './menu-burger.component.html',
   styleUrls: ['./menu-burger.component.scss']
 })
-export class MenuBurgerComponent {
+export class MenuBurgerComponent implements OnInit {
 
+  // Is menu burger visible
+  isVisible: boolean = false;
+
+  // Get list of values initialized in models/header.model.ts
+  values : HeaderParam = LIST_HEADER_VALUES[Number(this.isVisible)];
+
+  // Init an empty list of menu
+  menus: Menu[] = [];
+
+  constructor(private MenuService: MenuService) {}
+
+  ngOnInit(): void {
+    // Get values of burger menu or bottom navigation menu
+    this.menus = this.MenuService.getMenus();
+  }
+
+  toggleIsVisible() {
+    this.isVisible = !this.isVisible;
+    this.values = LIST_HEADER_VALUES[Number(this.isVisible)];
+  }
 }
